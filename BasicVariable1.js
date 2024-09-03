dee585f686ed2e36c8b00b6c3e043e106ed001e2
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2024-09-03 13:50:10 +0000

    first-learn-commit

---
+++ BasicVariable1.js
@@ -0,0 +1,28 @@
+// console.log("Hello we start our practice in java script");
+
+const user_Name = "Mahfooz";
+let email_Id = "mahfooz@google.com";
+var password = "12345";
+
+city = "Delhi";
+
+// const user_Name = "RAJ";  Note give error they not change
+
+ email_Id = "raj@google.com"; 
+ password = "123456"; 
+ city = "Bengluru";  // variable declare  like this but suggest not good behaviour to decide;
+ 
+ let accout_Id;   // here memory create space but undefind remember
+
+// console.log(user_Name);
+// console.log(email_Id);
+// console.log(password);
+// console.log(city);
+
+/*
+Note - preferd do not use var due to 
+       block scope and functional scope.....
+*/
+
+console.table([user_Name, email_Id, password, city]);
+
---
+++ DataTypes2.js
@@ -0,0 +1,14 @@
+ //"use strict";    // treate all code javascript new version
+ //alert("Hello ");  // here give error due to write code in 
+                     // nodejs not in browser please remember
+
+ // console.log("Hello"); console.log("Hello");  // that code right but here readbility essuse
+                                              //  please code clean
+  // Data type
+//   Number
+//   BigInt
+//   String
+//   Boolean
+//   null
+//   undefined
+//   Symbol
