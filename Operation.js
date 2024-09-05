72c9baa2c7d3919eb28234e1c42c2bdb0fe565fb
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2024-09-05 06:20:00 +0000

    Operation study practice compleated

---
+++ Operation.js
@@ -0,0 +1,114 @@
+  
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2%2);
+// console.log(2**2);
+
+// let value = 3;
+// let new_value = -value;
+// console.log(new_value);
+
+// str = "mahfooz";
+// str2 = " Alam";
+
+// str3  = str + str2;
+// console.log(str3);
+
+// // Investigation
+// let val1 = 1 + "3";
+// let val2 = "2" + 1;
+// let val3 = "3" +  "3";
+// let val  = 2 + 1;
+
+//  console.log(val);
+//  console.log(val1);
+//  console.log(val2);
+//  console.log(val3);
+ 
+//  console.log(typeof val);
+//  console.log(typeof val1);
+//  console.log(typeof val2);
+//  console.log(typeof val3);
+
+/*   output -
+4
+0
+1
+4
+0
+4
+-3
+mahfooz Alam
+3
+13
+21
+33
+number
+string
+string
+string
+*/
+ 
+
+val = "1" + 3 + 3;  // output 133   just for understanding not use because not good way
+val2 = 3 + 3 + "1"; // output 61  remembr
+
+
+// console.log(val);
+// console.log(val2);
+
+
+// console.log(+true); // true  but not use industry standerd
+// console.log(true+); // error
+
+
+// let num1, num2, num3;
+
+// num1 = num2  = num3  = 2+2;  // readability essue you write code but  not good behaviour of progarammer
+// console.log(num3);
+// console.log(num1);
+
+
+
+let prcedence = 100;  
+prcedence++;
+console.log(prcedence);
+
+let prcedence2 = 100;  
+++prcedence2;
+console.log(prcedence2);
+
+let prcedence3= 100;
+prcedence2--;
+console.log(prcedence3);
+
+let prcedence4 = 100;
+--prcedence2;
+console.log(prcedence4);
+
+
+  /*
+    output =>
+        
+      101
+      101
+      100
+      100  
+
+  */
+
+
+
+ // go mdn please read type convesion
+
+
+
+ 
+ 
+
+ 
+ 
+
+
---
+++ TypeConversion.js
@@ -0,0 +1,63 @@
+// let score = "33";
+// console.log(score);
+// console.log(typeof score);
+
+
+// let conert_Number = Number(score);
+// console.log(conert_Number);
+// console.log(typeof conert_Number);
+
+// let score2 = "33aa";
+// console.log(typeof score2);
+// console.log(score2);
+
+// let score3 = null;
+// let valu_null = Number(score3);
+// console.log(valu_null);
+// console.log(typeof valu_null);
+
+/*
+   Output - 
+
+   string
+   33aa
+   0     // if you match temprature than be in essue 
+   number
+*/
+
+// let score3 = undefined;
+// let value_undefind = Number(score3);
+// console.log(value_undefind);
+// console.log(typeof value_undefind);
+
+/*
+  output - 
+
+  NaN
+  number
+
+  "33" => 33
+  "33aa" => NaN
+  true = 1, false = 0
+*/
+
+// let isLoggedin = "";   // "" -> convert  false // "Mahfooz" => true
+
+// let boolean_isLoggedin = Boolean(isLoggedin);
+// console.log(boolean_isLoggedin);
+// console.log(typeof boolean_isLoggedin);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
