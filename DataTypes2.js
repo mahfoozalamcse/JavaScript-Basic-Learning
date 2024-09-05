77a1f83b7b4375c119fbbed56ade94c873addeec
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2024-09-05 04:23:40 +0000

    DataType Complete

--- DataTypes2.js
+++ DataTypes2.js
@@ -5,10 +5,29 @@
  // console.log("Hello"); console.log("Hello");  // that code right but here readbility essuse
                                               //  please code clean
   // Data type
-//   Number
-//   BigInt
-//   String
-//   Boolean
-//   null
-//   undefined
-//   Symbol
+//   number   0 to 53 power
+//   bigint - 0n (long number)
+//   string  - ""
+//   boolean  - T/F
+//   null    - standlone value  // also it have object
+//   undefined - value not assighn space create
+//   symbol   - unique value use
+
+// object
+
+console.log(typeof null);   
+console.log(typeof undefined);
+console.log(typeof BigInt);
+console.log(typeof Symbol);
+
+/*
+   output :-
+
+   object
+   undefined
+   function
+   function
+*/
+
+
+    // please mahfooz go ecma or mdn to explore more about dataTypes
