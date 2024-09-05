8fa19ed274d8d1eaa17b9ea8ce113fbec0ef1fb7
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2024-09-05 16:33:14 +0000

    change

--- 02Basic-Learning-js/Array2.js
+++ 02Basic-Learning-js/Array2.js
@@ -1,17 +1,17 @@
-// const marvel_heros = ["bahubali", "Ironman", "kgf"]
-// const dc_heros = ["superman", "fastman", "batman"]
+const marvel_heros = ["bahubali", "Ironman", "kgf"]
+const dc_heros = ["superman", "fastman", "batman"]
 
-// marvel_heros.push(dc_heros)
+marvel_heros.push(dc_heros)
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+console.log(marvel_heros);
+console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);
 
-// const all_new_heros = [...marvel_heros, ...dc_heros]   // spread operator
+const all_new_heros = [...marvel_heros, ...dc_heros]   // spread operator
 
-// console.log(all_new_heros);
+console.log(all_new_heros);
 
 // const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [8, 9]]]
 
