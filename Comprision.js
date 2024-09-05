61bef1edaec275b716111b6b87fd3c44cd7d50e2
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2024-09-05 07:11:04 +0000

    String Practice Completed need go mdn and browser window explore more

---
+++ Comprision.js
@@ -0,0 +1,34 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+// console.log(2==="2");
+// console.log(2 == "2");
+
+/*
+
+ console.log("2" > 1);
+ console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+  output - 
+  true
+  true
+  false
+  false
+  true
+  false
+  false
+  false
+
+*/
+
+console.log("3" === 3);  // false // strict check ===
---
+++ StringStudy.js
@@ -0,0 +1,69 @@
+/*
+const user_Name = "Mahfooz"
+const repoCount = 50
+
+console.log(user_Name + repoCount + " Value");  // concate 
+
+console.log(`Hello my name is ${user_Name} and my repo count is ${repoCount}`);
+
+const player_Name = new String('Mahfooz-top-org')
+
+console.log(player_Name[0]);
+console.log(player_Name.__proto__);
+
+
+   output - 
+   Mahfooz50 Value
+   Hello my name is Mahfooz and my repo count is 50
+   M
+   {} // when console than give {} because you in node server in browser give multiple function
+*/
+
+
+/*
+
+const user_Name = "Mahfooz";
+
+console.log(user_Name.length);
+console.log(user_Name.toUpperCase());
+console.log(user_Name.charAt(2));
+console.log(user_Name.indexOf('z'));
+
+const newString = user_Name.substring(0, 4)
+console.log(newString);
+
+const anotherString = user_Name.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   mahfooz    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+  output - 
+  7
+MAHFOOZ
+h
+6
+Mahf
+Mahf
+   mahfooz    
+mahfooz
+
+*/
+
+/*
+
+const user_Name = 'Mahfooz-google-com'
+const url = "https://github.com/mahfoozalamcse/hello1-git"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('hello1-git'))
+
+console.log(user_Name.split('-'));
+
+   Output - 
+   https://github.com/mahfoozalamcse/hello1-git
+true
+[ 'Mahfooz', 'google', 'com' ]
+ 
+ */
